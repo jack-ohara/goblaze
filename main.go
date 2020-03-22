21b4222c0caf600918851e0b539b70839dbbe6f4
Jack O'Hara <jack.ohara@protonmail.com>
2020-03-22 11:19:21 +0000

    Added dotenv support and removed the secrets from main.go

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/jack-ohara/goblaze/goblaze"
 	"github.com/jack-ohara/goblaze/goblaze/fileuploader"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	authorizationInfo := goblaze.GetAccountAuthorization("7f3a2586df8e", "0000ac10cd67b60d1e4333a8cfff77aee02297a008")
+	err := godotenv.Load()
 
-	fileuploader.UploadFile("/home/jack/helloworld.txt", authorizationInfo, "67cf33ca2205c8667d0f081e")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authorizationInfo := goblaze.GetAccountAuthorization(os.Getenv("KEY_ID"), os.Getenv("APPLICATION_ID"))
+
+	fileuploader.UploadFile("/home/jack/helloworld.txt", authorizationInfo, os.Getenv("BUCKET_ID"))
 }
