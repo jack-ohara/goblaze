03789c98b86a8106b5618a8f057875835e652cbe
Jack O'Hara <jack.ohara@protonmail.com>
2020-04-17 11:35:50 +0100

    Added the proper command line arguments for uploading and downloading

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
@@ -25,17 +26,68 @@ func main() {
 
 	configValues := getEnvironmentVariables()
 
-	authorizationInfo := accountauthorization.GetAccountAuthorization(configValues.KeyID, configValues.ApplicationKey)
+	uploadCommand := flag.NewFlagSet("upload", flag.ExitOnError)
+	uploadDirectory := uploadCommand.String("dir", "", "Identifies the directory to upload")
 
-	goblaze.UploadDirectory(os.Args[1], configValues.EncryptionPassphrase, configValues.BucketID, authorizationInfo)
+	downloadCommand := flag.NewFlagSet("download", flag.ExitOnError)
+	downloadDirectory := downloadCommand.String("dir", "", "Identifies the directory to download from backblaze")
+	downloadDestination := downloadCommand.String("dest", ".", "Identifies the location on disk that the downloaded files will be written to")
+	downloadWriteMode := downloadCommand.Int("write-mode", 1, "Value of 0: Does not overwrite the file if it already exists\nValue of 1: Overwrites existing files if the downloaded file is more recent\nValue of 2: Overwrites any existing files")
 
-	downloadOptions := goblaze.DownloadOptions{
-		DirectoryName:   "/home/jack/Documents/Backup-Test/",
-		TargetDirectory: "/home/jack/Backup-Downloaded",
-		WriteMode:       goblaze.OverwriteOldFiles,
+	switch os.Args[1] {
+	case "upload":
+		uploadCommand.Parse(os.Args[2:])
+
+		if len(uploadCommand.Args()) > 0 {
+			log.Fatalln("Unexpected arguments to goblaze upload: ", uploadCommand.Args())
+		}
+
+		fileInfo, err := os.Stat(*uploadDirectory)
+
+		if os.IsNotExist(err) {
+			log.Fatalln("Directory does not exist: ", *uploadDirectory)
+		}
+
+		if !fileInfo.IsDir() {
+			log.Fatalln("Expected 'dir' argument to point to a directory but it is a file: ", *uploadDirectory)
+		}
+
+		authorizationInfo := accountauthorization.GetAccountAuthorization(configValues.KeyID, configValues.ApplicationKey)
+
+		goblaze.UploadDirectory(*uploadDirectory, configValues.EncryptionPassphrase, configValues.BucketID, authorizationInfo)
+	case "download":
+		downloadCommand.Parse(os.Args[2:])
+
+		if len(downloadCommand.Args()) > 0 {
+			log.Fatalln("Unexpected arguments to goblaze download: ", downloadCommand.Args())
+		}
+
+		fileInfo, err := os.Stat(*downloadDestination)
+
+		if os.IsNotExist(err) {
+			log.Fatalln("Destination directory does not exist: ", *downloadDestination)
+		}
+
+		if !fileInfo.IsDir() {
+			log.Fatalln("Expected 'dest' argument to point to a directory but it is a file: ", *downloadDestination)
+		}
+
+		if *downloadWriteMode != 0 && *downloadWriteMode != 1 && *downloadWriteMode != 2 {
+			log.Fatalln("Invalid value for write-mode: ", *downloadWriteMode)
+		}
+
+		authorizationInfo := accountauthorization.GetAccountAuthorization(configValues.KeyID, configValues.ApplicationKey)
+
+		downloadOptions := goblaze.DownloadOptions{
+			DirectoryName:   *downloadDirectory,
+			TargetDirectory: *downloadDestination,
+			WriteMode:       goblaze.FileWriteMode(*downloadWriteMode),
+		}
+
+		goblaze.DownloadDirectory(downloadOptions, configValues.EncryptionPassphrase, authorizationInfo)
+	default:
+		log.Fatalln("Expected a subcommand of 'upload' or 'download'")
 	}
-
-	goblaze.DownloadDirectory(downloadOptions, configValues.EncryptionPassphrase, authorizationInfo)
 }
 
 func getEnvironmentVariables() configurationValues {
